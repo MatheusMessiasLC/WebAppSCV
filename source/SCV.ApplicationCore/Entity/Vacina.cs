7a7f6b92e93e0739dfc1c77b3e82919966ca2220
LucasALC <lucasandrelescanocruz@gmail.com>
2018-04-11 11:05:18 -0400

    Add project files.

---
+++ source/SCV.ApplicationCore/Entity/Vacina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCV.ApplicationCore.Entity
+{
+    public class Vacina
+    {
+        public Vacina()
+        {
+
+        }
+        public int VacinaId { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
